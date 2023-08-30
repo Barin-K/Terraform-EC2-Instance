4ff4fa4488f40efa533fee207c7793273b577b41
Barry Kane <barrybargz@Barrys-MacBook-Pro.local>
2023-08-30 11:00:32 +0100

    EC2 Commit

---
+++ provider.tf
@@ -0,0 +1,13 @@
+#AWS Provider Configuration
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
